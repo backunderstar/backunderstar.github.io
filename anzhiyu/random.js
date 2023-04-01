df343565783efe032d7c7d657e46e45839f3c8aa
backunderstar <89375248+backunderstar@users.noreply.github.com>
2023-04-01 13:04:22 +0000

    fix Updated By buckunderstar

--- anzhiyu/random.js
+++
@@ -1,1 +0,0 @@
-var posts=["/common_shell/","/genshin_sayings/","/hexo_create/","/mc_server/","/ubuntu_root/","/yunzai_docker/","/yunzai_win/","/zhanwei/"];function toRandomPost(){pjax.loadUrl("/"+posts[Math.floor(Math.random()*posts.length)])}
